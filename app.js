6831b0e303fae154850070d7ad8ee4a212da1ca6
nicoleavieir4 <nicoleavieira@icloud.com>
2023-03-15 16:04:38 -0300

    caminhando com os dados

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const server = express();
 const port = 3000;
+const cities = [
+  {
+    name: "CAMPINAS",
+    temperature: 32,
+  },
+  {
+    name: "SÃO PAULO",
+    temperature: 22,
+  },
+  {
+    name: "CURITIBA",
+    temperature: 2,
+  },
+  {
+    name: "ALAGOAS",
+    temperature: 39,
+  },
+];
 
-server.get("/pokemon-by-city", (req, res) => {
-  const { city } = req.query;
-  console.log(req.query)
+server.get("/pokemon/search", (req, res) => {
+  const { city } = req.query; //recebe o valor do client
+  const cityResult = cities.find((localCity) => localCity.name == city); // verificando se a cidade recebida existe no array (registrada)
+  const response = cityResult
+    ? { data: cityResult, status: 200 }
+    : { data: { error: "city not found" }, status: 404 };
 
-  return res.json({
-    city
-  });
+  return res.status(response.status).json(response.data);
 });
-
-server.get("/pokemon/:city", (req, res) => {
-    const { city } = req.params;
-    console.log(req.params)
-  
-    return res.json({
-      city
-    });
-  });
 
 server.listen(port, () => {
   console.log(`App listening on port ${port}`);
