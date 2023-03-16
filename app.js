3d668811ae6b3e29673c3d62f44e927872bfeef2
nicoleavieir4 <nicoleavieira@icloud.com>
2023-03-16 15:25:08 -0300

    integrando API

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
 const express = require("express");
+const service = require("./service");
+
 const server = express();
 const port = 3000;
 const cities = [
   {
     name: "CAMPINAS",
     temperature: 32,
+    isRaining: true,
   },
   {
     name: "SÃO PAULO",
     temperature: 22,
+    isRaining: true,
   },
   {
     name: "CURITIBA",
     temperature: 2,
+    isRaining: false,
   },
   {
     name: "ALAGOAS",
     temperature: 39,
+    isRaining: false,
   },
 ];
 
 server.get("/pokemon/search", (req, res) => {
   const { city } = req.query; //recebe o valor do client
   const cityResult = cities.find((localCity) => localCity.name == city); // verificando se a cidade recebida existe no array (registrada)
-  const response = cityResult
-    ? { data: cityResult, status: 200 }
-    : { data: { error: "city not found" }, status: 404 };
+  if (cityResult) {
+    const type = service(cityResult.temperature, cityResult.isRaining);
 
-  return res.status(response.status).json(response.data);
+    const response = {
+      name: cityResult.name,
+      temperature: cityResult.temperature,
+      isRaining: cityResult.isRaining,
+      type: type,
+    };
+
+    return res.json(response);
+  }
+
+  const statusRet = res.response
+  ? { data: response, status: 200 }
+  : { data: { error: "city not found" }, status: 404 };
+
+  return res.status(statusRet.status).json(statusRet.data);
+
+  // return res.json(cityResult);
 });
 
 server.listen(port, () => {
---
+++ service.js
@@ -0,0 +1,23 @@
+function wetherToPokemonType(temperature, isRain) {
+  if (isRain) return "eletric";
+
+  if (temperature < 5) {
+    return "ice";
+  } else if (temperature >= 5 && temperature < 10) {
+    return "water";
+  } else if (temperature >= 12 && temperature < 15) {
+    return "grass";
+  } else if (temperature >= 15 && temperature < 21) {
+    return "ground";
+  } else if (temperature >= 23 && temperature < 27) {
+    return "bug";
+  } else if (temperature >= 27 && temperature <= 33) {
+    return "rock";
+  } else if (temperature > 33) {
+    return "fire";
+  } else {
+    return "normal";
+  }
+}
+
+module.exports = wetherToPokemonType
